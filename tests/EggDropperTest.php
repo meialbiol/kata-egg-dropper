1a4001dca5fe71687ae6733fc6a42ae3666fa591
lovaina <meialbiol@gmail.com>
2018-02-21 19:43:25 +0100

    Existe Eggdropper class

---
+++ tests/EggDropperTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class EggDropperTest extends PHPUnit_Framework_TestCase {
+
+    public function test_EggDropper_exists(){
+        
+    }
+    
+}
+ 
