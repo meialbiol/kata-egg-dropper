fa30aaa6a60f399b3cb70abdcefe10fae47a6068
lovaina <meialbiol@gmail.com>
2018-02-21 19:49:07 +0100

    Iniciada clase EggDropper

--- src/EggDropper.php
+++ src/EggDropper.php
@@ -1,1 +1,6 @@
 <?php
+namespace Kata;
+
+class EggDropper{
+
+}
--- tests/EggDropperTest.php
+++ tests/EggDropperTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests;
 
+use Kata\EggDropper;
 use PHPUnit_Framework_TestCase;
 
 class EggDropperTest extends PHPUnit_Framework_TestCase {
 
     public function test_EggDropper_exists(){
-        
+        $eggDropper = new EggDropper();
+        $this->assertInstanceOf('Kata\EggDropper', $eggDropper);
     }
     
 }
